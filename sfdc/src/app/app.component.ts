8f61327f9ae2f1a96ee2ec3ec6ecb79698cf44a6
Andrey Ryzhkov <aryzhkov@aquivalabs.com>
2019-04-04 22:23:25 +0300

    Init commit

--- sfdc/src/app/app.component.ts
+++ sfdc/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, TimeInterval, interval, of, from } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { jsforce } from 'jsforce';
+// declare var require: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'heroku-sfdc-status';
+  private baseUrl = 'https://api.status.salesforce.com/v1/';
+  private sfdcProduct = 'Service_Cloud' + '/';
+  private products = [];
+  private pollInterval = 2 * 60 * 10;
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get(this.baseUrl + 'products' + '/' + this.sfdcProduct).subscribe((res: any[]) => {
+        this.products = res['Instances'].slice(0, 10);
+    });
+  }
+
+    pollStatus() {
+        interval(this.pollInterval)
+        .pipe(
+            mergeMap(
+                () => this.httpClient.get(this.baseUrl + 'products' + '/' + this.sfdcProduct)
+            )
+        )
+        .subscribe(data => {
+            console.log(data);
+            this.products = data['Instances'].slice(0, 10);
+        });
+    }
 }
